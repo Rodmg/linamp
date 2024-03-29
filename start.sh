119a9a8089c0a9f1bbf72bb3fbe79d7838a8df74
Rodrigo Mendez <rodmg4@gmail.com>
2024-03-29 13:23:19 -0600

    Start script improv

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 source venv/bin/activate
-PYTHONPATH=python ./player
+DISPLAY=:0 PYTHONPATH=python $SCRIPT_DIR/player
