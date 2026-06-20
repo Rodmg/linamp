1c7b28cd7fc40aca176a4b3ccad08c1e60c91e08
Rodrigo Mendez <rodrigo@linamp.org>
2026-06-20 12:24:17 -0600

    chore: Update changelog, add all deps to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ IFS=$'\n\t'
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-# Setup build dependencies for native CD playback and Python environment for BT/Spotify
-sudo apt-get install build-essential \
-    libcdio-dev libcdio-paranoia-dev libdiscid-dev \
-    python3-pip python3-full python3-dev -y
+# Setup build dependencies (see README.md Development section)
+sudo apt-get install build-essential cmake \
+    qt6-base-dev qt6-base-dev-tools qt6-multimedia-dev qtcreator \
+    libtag1-dev libasound2-dev libpulse-dev libpipewire-0.3-dev libdbus-1-dev \
+    libcdio-dev libcdio-paranoia-dev libcdio-utils libdiscid-dev \
+    python3-pip python3-full python3-dev wlr-randr -y
 
 python3 -m venv venv
 source venv/bin/activate
